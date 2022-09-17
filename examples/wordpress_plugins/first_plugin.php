f3575d19f61451f08694a7d6370a4a8e63426120
Aaron Jones <ajones061392@yahoo.com>
2022-09-17 13:25:32 -0700

    Added some examples for a javascript tutorial, I'm going over and added some code to my first WordPress plugin.

--- examples/wordpress_plugins/first_plugin.php
+++ examples/wordpress_plugins/first_plugin.php
@@ -1,3 +1,18 @@
 <?php
+    /**
+     * Plugin Name:  FirstPlugin
+     * Plugin URI:   https://cogswebdevelopmentandhosting.com/plugins/first-plugin/
+     * Description:  This is my first plugin and it will take a string and output a string on 
+     *               WordPress. 
+     * Version:      0.0.1
+     * Author:       Aaron Jones
+     * Author URI:   https://github.io/bigboss424/jones-resume
+     * License:      GPL v2 or later
+     * License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+     * Update URI:   https://cogswebdevelopmentandhosting.com/plugins/
+     * 
+    */
     
+    function 
+
 ?>
