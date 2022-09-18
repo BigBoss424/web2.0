a9478c1182eac4d68560526dd8ddd4143bc5b73d
Aaron Jones <ajones061392@yahoo.com>
2022-09-18 14:46:35 -0700

    Finished the navbar example

--- examples/04-navbar/setup/app.js
+++ examples/04-navbar/setup/app.js
@@ -3,3 +3,14 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector(".links");
+
+//navigation toggle
+
+navToggle.addEventListener("click", function(){
+
+    links.classList.toggle("show-links");
+});
+
