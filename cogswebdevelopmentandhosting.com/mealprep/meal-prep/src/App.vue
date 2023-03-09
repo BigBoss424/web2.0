6407d12a11db5fffbf35ad7cfbead81b21fe2c3b
ajones424 <ajones061392@cogswebdevelopmentandhosting.com>
2023-03-09 14:17:30 -0800

    Added some new changes to the web2.0 as I attempt to implement Firebase.

---
+++ cogswebdevelopmentandhosting.com/mealprep/meal-prep/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+      <v-content transition="slide-x-transition">
+          <router-view></router-view>
+      </v-content>
+  </v-app>
+</template>
+
+<script>
+    export default; {
+        name: 'Meal-Prep'
+    };
+</script>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
--- cogswebdevelopmentandhosting.com/v2/app.js
+++ cogswebdevelopmentandhosting.com/v2/app.js
@@ -45,42 +45,5 @@ app.get('/features', function(req, res){
 app.get('/pricing', function(req, res){
     res.render('pricing');
 });
-app.post('/contact/send', function(req, res){
-    var transporter = nodemailer.createTransport({
-        service: 'gmail', 
-        auth: {
-            user: 'rsaiyan4444@gmail.com',
-            pass: ''
-        }
-    });
-
-    var mailOptions = {
-		from: 'Aaron Jones <rsaiyan4444@gmail.com>',
-		to: 'ajones061392@yahoo.com',
-		subject: 'Website Submission',
-		text: 'You have a submission with the following details... Name: '+req.body.name+'Email: '+req.body.email+'Message: '+req.body.message,
-		html: '<p>You have a submission with the following details...</p><ul><li>'+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
-	};
-
-	transporter.sendMail(mailOptions, function(error, info){
-		if(error){
-			console.log(error);
-			res.redirect('/');
-		} else {
-			console.log('Message Sent: '+info.response);
-			res.redirect('/');
-		}
-        
-    });
-
-    var mailOptions = {
-        from: 'Aaron Jones <ajones061392@yahoo.com>',
-        to: 'rsaiyan4444@gmail.com',
-        subject: 'Website Submission',
-        text: 'You have a website submission with the following details... Name: ' +
-            req.body.name+'Email: '+req.body.email+'Message: '+req.body.message,
-        html: '<p>You have a submission with the following details...</p><ul><li>'+req.body.name+'</li><li>Email: '+req.body.email+'<li><li>Message: '+req.body.message+'</li></ul>'
-    };//end contact 
 
 
-});
