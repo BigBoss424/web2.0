977bfb6bb7229e75fa394d4e9450bdc69611eba7
ajones424 <ajones061392@cogswebdevelopmentandhosting.com>
2023-03-13 13:20:33 -0700

    Added some functions to the Firebase Functions codebase for the website.

--- cogswebdevelopmentandhosting.com/functions/index.js
+++ cogswebdevelopmentandhosting.com/functions/index.js
@@ -1,2 +1,16 @@
-//const functions = require("firebase-functions");
+const functions = require("firebase-functions");
 
+exports.helloWorld = functions.https.onRequest((req, res) => {
+    var helloWorld = "Hello World"
+    res.status(200).send(
+    "<!doctype html>
+        <html>
+          <head>
+                <title>Test Function</title>
+            </head>
+            <body>
+                ${'Hello World'}
+            </body>
+        </html>")
+    )
+});
