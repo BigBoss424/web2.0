c8926bcb6524b11650ce8f330cb12d9718f2349f
ajones424 <ajones061392@cogswebdevelopmentandhosting.com>
2023-03-16 17:52:26 -0700

    Starting over again.

---
+++ cogswebdevelopmentandhosting.com/cogsSite/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
